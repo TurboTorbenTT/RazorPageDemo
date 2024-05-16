bc72ad833119be8247bf907987d6aaa753e3c03e
TurboTorbenTT <peterbek104@hotmail.com>
2024-05-16 22:30:16 +0200

    Scaffold all the shit

---
+++ RazorWebApp/Models/IceCream.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorWebApp.Models;
+
+public class IceCream
+{
+  public Guid? Id { get; set; }
+  public string Name { get; set; }
+  public decimal Price { get; set; }
+  public bool OnSale { get; set; }
+  public int SaleDiscount { get; set; }
+}
